705fc4851d7b3f4c9bee484d804ac13905a9c1a5
Sergio Lopez <sergiolopez403@gmail.com>
2023-05-25 13:46:06 -0300

    instalamos react-bootstrap

--- difatec/src/App.js
+++ difatec/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { AddCategory } from "./pages/add-categories";
+import { AddTask } from "./pages/add-task";
+import { Login } from "./pages/login";
+import { Signup } from "./pages/signup";
+import { Home } from "./pages/home";
 
 function App() {
-  return <>Funciona</>;
+  return (
+    <div className="App">
+      {/* <AddCategory />
+      <AddTask />
+      <Login />
+      <Signup /> */}
+      <Home />
+    </div>
+  );
 }
 
 export default App;
